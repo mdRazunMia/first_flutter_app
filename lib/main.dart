286a88903039adb1eccc8aac98262490ddc1bf94
Md. Razun Mia <razun.neub.cse@gmail.com>
2020-11-04 08:17:58 +0600

    Drawer has been added in the project

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,50 @@ class TabState extends State<TabsExample> {
     return DefaultTabController(
         length: 3,
         child: new Scaffold(
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Text('List of Menu'),
+                    decoration: BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                ),
+                ListTile(
+                  title: Text('Home'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('About'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('contact'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
           appBar: AppBar(
             title: Text('My First Flutter App'),
             bottom: TabBar(
